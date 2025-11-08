61fa6c407294d5438a006496b3467d2285c0d16b
Ipshita3 <2024csipshita17813@poornima.edu.in>
2025-11-08 10:18:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function filterItems(category) {
+  const items = document.querySelectorAll('.gallery-grid .item');
+  items.forEach(item => {
+    if (category === 'all' || item.classList.contains(category)) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
